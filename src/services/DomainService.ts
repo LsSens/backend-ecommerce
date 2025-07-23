14d1c858e8de24749fc2acad33b5900a9ff9f469
Lucas Sens <lucassousasens@gmail.com>
2025-07-22 23:16:06 -0300

    feat: validate domain

--- src/services/DomainService.ts
+++ src/services/DomainService.ts
@@ -1,15 +1,45 @@
+import { promises as dns } from 'dns';
+
 export class DomainService {
     async verifyCompanyDomain(domain: string): Promise<any> {
         try {
-         const data = {
-          isValid: true,
-          isPointingToAWS: true,
-          domain,
-          status: 200
-         }
-          return data;
+            const nsRecords = await dns.resolveNs(domain);
+            
+            const correctNameservers = [
+                'ns-1465.awsdns-55.org',
+                'ns-1465.awsdns-55.org.',
+                'ns-543.awsdns-03.net',
+                'ns-543.awsdns-03.net.',
+                'ns-418.awsdns-52.com',
+                'ns-418.awsdns-52.com.',
+                'ns-1873.awsdns-42.co.uk',
+                'ns-1873.awsdns-42.co.uk.'
+            ];
+            
+            const isPointingToAWS = nsRecords.every(ns => 
+                correctNameservers.includes(ns)
+            );
+
+            if (isPointingToAWS) {
+                return {
+                    isValid: true,
+                    domain,
+                    status: 200
+                };
+            } else {
+                return {
+                    isValid: false,
+                    domain,
+                    status: 422
+                };
+            }
         } catch (error) {
-          throw error;
+            return {
+                isValid: false,
+                domain,
+                error: error.message,
+                status: 400
+            };
         }
-      } 
+    }
 }
